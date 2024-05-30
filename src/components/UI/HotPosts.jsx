aace0ea698e6e51985329daeed091235db3440b6
Jayson <rojasjay24@gmail.com>
2024-05-30 18:50:42 -0500

    Hot collections completed

---
+++ src/components/UI/HotPosts.jsx
@@ -0,0 +1,140 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Skeleton from "../UI/Skeleton";
+import { Link } from "react-router-dom";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+const HotPosts = () => {
+  const [hotPosts, setHotPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [settings, setSettings] = useState([]);
+
+  useEffect(() => {
+    async function fetchHotPosts() {
+      const { data } = await axios.get(
+        "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"
+      );
+      setHotPosts(data);
+      setLoading(false);
+    }
+    fetchHotPosts();
+  }, []);
+
+  useEffect(() => {
+    function SimpleSlider() {
+      const options = {
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />,
+        responsive: [
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+            },
+          },
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              infinite: true,
+            },
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+            },
+          },
+        ],
+      };
+      setSettings(options);
+    }
+    SimpleSlider();
+  }, []);
+
+  function NextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "flex", position: "absolute" }}
+        onClick={onClick}
+      />
+    );
+  }
+
+  function PrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "flex", position: "absolute" }}
+        onClick={onClick}
+      />
+    );
+  }
+
+  return (
+    <Slider {...settings}>
+      {loading
+        ? new Array(4).fill(0).map((_, index) => (
+            <div className="nft_coll" key={index}>
+              <div className="nft_wrap">
+                <Skeleton width="100%" height="200px" />
+              </div>
+              <div className="nft_coll_pp">
+                <Skeleton width="50px" height="50px" borderRadius="50%" />
+                <i className="fa fa-check"></i>
+              </div>
+              <div className="nft_coll_info">
+                <Skeleton width="100px" height="20px" />
+                <br />
+                <Skeleton width="60px" height="20px" />
+              </div>
+            </div>
+          ))
+        : hotPosts.map((item) => (
+            <div className="nft_coll">
+                  <div className="nft_wrap">
+                    <Link to="/item-details">
+                      <img
+                        src={item.nftImage}
+                        className="lazy img-fluid"
+                        alt=""
+                      />
+                    </Link>
+                  </div>
+                  <div className="nft_coll_pp">
+                    <Link to="/author">
+                      <img
+                        className="lazy pp-coll"
+                        src={item.authorImage}
+                        alt=""
+                      />
+                    </Link>
+                    <i className="fa fa-check"></i>
+                  </div>
+                  <div className="nft_coll_info">
+                    <Link to="/explore">
+                      <h4>{item.title}</h4>
+                    </Link>
+                    <span>ERC-{item.code}</span>
+                  </div>
+                </div>
+          ))}
+    </Slider>
+  );
+};
+
+export default HotPosts;
--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,32 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import "keen-slider/keen-slider.css";
-import { useKeenSlider } from "keen-slider/react";
-import Skeleton from "../UI/Skeleton";
+import React from "react";
+import HotPosts from "../UI/HotPosts";
 
 const HotCollections = () => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    async function getCollections() {
-      const responce = await axios.get(
-        `https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections`
-      );
-      setData(responce.data);
-      setLoading(false);
-    }
-    getCollections();
-  }, []);
-
-  const [sliderRef] = useKeenSlider({
-    loop: true,
-    slides: {
-      number: 6,
-    },
-  });
-
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -37,74 +12,7 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          <div ref={sliderRef} className="keen-slider">
-            <div className="keen-slider__slide">
-              {loading ? (
-                <>
-                  {new Array(4).fill(0).map((_, index) => (
-                    <div
-                      className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
-                      key={index}
-                    >
-                      <div className="nft_coll">
-                        <div className="nft_wrap">
-                          <Skeleton width="100%" height="200px" />
-                        </div>
-                        <div className="nft_coll_pp">
-                          <Skeleton
-                            width="50px"
-                            height="50px"
-                            borderRadius="50%"
-                          />
-                          <i className="fa fa-check"></i>
-                        </div>
-                        <div className="nft_coll_info">
-                          <Skeleton width="100px" height="20px" />
-                          <br />
-                          <Skeleton width="60px" height="20px" />
-                        </div>
-                      </div>
-                    </div>
-                  ))}
-                </>
-              ) : (
-                data.map((obj, index) => (
-                  <div
-                    className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
-                    key={index}
-                  >
-                    <div className="nft_coll">
-                      <div className="nft_wrap">
-                        <Link to="/item-details">
-                          <img
-                            src={obj.nftImage}
-                            className="lazy img-fluid"
-                            alt=""
-                          />
-                        </Link>
-                      </div>
-                      <div className="nft_coll_pp">
-                        <Link to="/author">
-                          <img
-                            className="lazy pp-coll"
-                            src={obj.authorImage}
-                            alt=""
-                          />
-                        </Link>
-                        <i className="fa fa-check"></i>
-                      </div>
-                      <div className="nft_coll_info">
-                        <Link to="/explore">
-                          <h4>{obj.title}</h4>
-                        </Link>
-                        <span>ERC-{obj.code}</span>
-                      </div>
-                    </div>
-                  </div>
-                ))
-              )}
-            </div>
-          </div>
+          <HotPosts />
         </div>
       </div>
     </section>
