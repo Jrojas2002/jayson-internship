dbc20f39193f4718dad8a9978e7b33893e2c41ce
Jayson <rojasjay24@gmail.com>
2024-06-03 21:18:51 -0500

    Item Details complete

--- src/components/UI/Post.jsx
+++ src/components/UI/Post.jsx
@@ -148,7 +148,7 @@ const Post = () => {
                   </div>
                 </div>
 
-                <Link to="/item-details">
+                <Link to={`/item-details/${item.nftId}`}>
                   <img
                     src={item.nftImage}
                     className="lazy nft__item_preview"
@@ -157,7 +157,7 @@ const Post = () => {
                 </Link>
               </div>
               <div className="nft__item_info">
-                <Link to="/item-details">
+                <Link to={`/item-details/${item.nftId}`}>
                   <h4>{item.title}</h4>
                 </Link>
                 <div className="nft__item_price">{item.price} ETH</div>
