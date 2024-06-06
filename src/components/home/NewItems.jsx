62b8a59471066defe9559e0106a0ab117c37639c
Jayson <rojasjay24@gmail.com>
2024-06-05 19:24:13 -0500

    animations complete

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Post from "../UI/Post";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const NewItems = () => {
 
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <section id="section-items" className="no-bottom">
+      <div data-aos="fade">
       <div className="container">
         <div className="row">
           <div className="col-lg-12">
@@ -15,6 +22,7 @@ const NewItems = () => {
           </div>
           <Post />
         </div>
+      </div>
       </div>
     </section>
   );
